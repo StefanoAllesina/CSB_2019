fe55dbbaf7dade8a800dffc3b7200177ab82e6a3
Stefano Allesina <sallesina@uchicago.edu>
2019-02-12 18:12:24 -0600

    small changes to week 6

--- week6/sandbox/parse_bioRXiv.py
+++ week6/sandbox/parse_bioRXiv.py
@@ -1,10 +1,7 @@
-#!/usr/bin/python
-
 import re # regular expressions
 import csv # manipulate csv files
 import os # interface with operating system
 from bs4 import BeautifulSoup # HTML parser
-import sys # command line arguments
 
 def process_file(fname):
     # this shows how simple it is to parse html (xml, json, etc.) files
@@ -16,32 +13,23 @@ def process_file(fname):
     results = []
     papers = soup.findAll("div", {"class": "highwire-article-citation highwire-citation-type-highwire-article tooltip-enable"})
     for paper in papers:
-        title = paper.findAll("span", {"class": "highwire-cite-title"})[0]
-        title = title.text.strip()
+        title = paper.attrs['title']
+        date, paperid = re.findall(r'/early/(\d{4}/\d+/\d+)/(\d+)', paper.attrs['data-apath'])[0]
         last_names = paper.findAll("span", {"class": "nlm-surname"})
-        last_names = [n.text.strip() for n in last_names]
-        num_authors = len(last_names)
-        tmp = paper.findAll("span", {"class": "highwire-cite-title"})[0]
-        dateurl = tmp.findAll('a')[0].get('href')
-        date, paperid = re.findall(r'/content/early/(\d{4}/\d+/\d+)/(\d+)', dateurl)[0]
+        num_authors = len(last_names)      
         results.append([str(paperid), str(date), str(num_authors), str(title)])
     return results
 
-def process_all(my_category):
-    # open csv file...
-    with open('bioRXiv.csv', 'w') as fo:
-       writer = csv.writer(fo)
-       # and write header
-       writer.writerow(['category', 'paperid', 'date', 'num_authors', 'title'])
-       # process each file
-       all_files = os.listdir('data/' + my_category)
-       for f in all_files:
-           res = process_file('data/' + my_category + '/' + f)
-           for r in res:
-               writer.writerow([my_category] + r)
-
-# If run from command line, launch category 
-# stored in first argument
-if __name__ == "__main__":
-   process_all(sys.argv[1])
+my_category = 'ecology'
+# open csv file...
+with open('bioRXiv.csv', 'w') as fo:
+    writer = csv.writer(fo)
+    # and write header
+    writer.writerow(['category', 'paperid', 'date', 'num_authors', 'title'])
+    # process each file
+    all_files = os.listdir('data/' + my_category)
+    for f in all_files:
+        res = process_file('data/' + my_category + '/' + f)
+        for r in res:
+            writer.writerow([my_category] + r)
 
